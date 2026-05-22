87be1fa424fde1f3dfbaeb6b7843e53ef20571e8
rannn <151916275+Rifki-Adi@users.noreply.github.com>
2026-05-22 16:39:20 +0700

    Update lihat_tiket.php

--- lihat_tiket.php
+++ lihat_tiket.php
@@ -38,6 +38,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lihat Tiket - <?= $tiket['kode_booking'] ?></title>
+    <link rel="icon" href="logo 1.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
